12a7da8dac92d888a620002e8d57a1c91294c9da
Kim Nguyen <design@kimisgold.com>
2012-11-20 21:03:36 -0500

    Change sass output directory.

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
