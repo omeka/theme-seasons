eac8c8571ae94de05b9888719c6b30833a53f804
Kim Nguyen <design@kimisgold.com>
2013-01-24 11:09:41 -0500

    Move compass configuration file.

--- css/config.rb
+++ css/config.rb
@@ -3,8 +3,8 @@ require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "css/sass"
+css_dir = "/"
+sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
