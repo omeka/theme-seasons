a532e2e1ed4ca5edb01c1c1e36924701deb16681
Kim Nguyen <design@kimisgold.com>
2013-01-24 00:07:44 -0500

    Move sass folder to within the css folder.

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require 'susy'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "css/sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
