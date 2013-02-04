a4291f7f8dff4cc40a874e94ff01f6e1b469ab4f
Kim Nguyen <design@kimisgold.com>
2013-02-04 12:57:43 -0500

    Make headings consistent, and use item image gallery to display non-image item icons.

--- items/show.php
+++ items/show.php
@@ -13,15 +13,15 @@
     <!-- The following returns all of the files associated with an item. -->
     <?php if (metadata('item', 'has files')): ?>
     <div id="itemfiles" class="element">
-        <h3><?php echo __('Files'); ?></h3>
-        <div class="element-text"><?php echo files_for_item(); ?></div>
+        <h2><?php echo __('Files'); ?></h2>
+        <?php echo item_image_gallery(); ?>
     </div>
     <?php endif; ?>
 
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->
     <?php if (metadata('item', 'Collection Name')): ?>
     <div id="collection" class="element">
-        <h3><?php echo __('Collection'); ?></h3>
+        <h2><?php echo __('Collection'); ?></h2>
         <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
     </div>
     <?php endif; ?>
@@ -29,14 +29,14 @@
     <!-- The following prints a list of all tags associated with the item -->
     <?php if (metadata('item', 'has tags')): ?>
     <div id="item-tags" class="element">
-        <h3><?php echo __('Tags'); ?></h3>
+        <h2><?php echo __('Tags'); ?></h2>
         <div class="element-text"><?php echo tag_string('item'); ?></div>
     </div>
     <?php endif;?>
 
     <!-- The following prints a citation for this item. -->
     <div id="item-citation" class="element">
-        <h3><?php echo __('Citation'); ?></h3>
+        <h2><?php echo __('Citation'); ?></h2>
         <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
     </div>
 
