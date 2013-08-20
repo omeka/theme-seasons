90d74bea50bb09f37e5a2cd79c686d6543ba0db0
Kim Nguyen <design@kimisgold.com>
2013-08-20 15:46:52 -0400

    Fix winter exhibit page navigation colors, fix margins for new exhibit blocks.

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -1,9 +1,11 @@
 <?php
+foreach ($layout_styles as $css_url):
+    queue_css_url($css_url);
+endforeach;
 echo head(array(
     'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
     'bodyclass' => 'exhibits show'));
 ?>
-
 <nav id="exhibit-pages">
     <?php echo exhibit_builder_page_nav(); ?>
 </nav>
@@ -14,7 +16,9 @@ echo head(array(
     <?php echo exhibit_builder_child_page_nav(); ?>
 </nav>
 
+<div role="main">
 <?php exhibit_builder_render_exhibit_page(); ?>
+</div>
 
 <div id="exhibit-page-navigation">
     <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
