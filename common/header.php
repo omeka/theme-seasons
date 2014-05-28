44817663f1b86544d935263c278c345fc0115271
Kim Nguyen <design@kimisgold.com>
2014-05-28 13:23:39 -0400

    Turn on site-wide search by default.

--- common/header.php
+++ common/header.php
@@ -44,7 +44,7 @@
                 <?php echo link_to_home_page(theme_logo()); ?>
             </div>
             <div id="search-container">
-                <?php if (get_theme_option('use_advanced_search')): ?>
+                <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
                 <?php echo search_form(array('show_advanced' => true)); ?>
                 <?php else: ?>
                 <?Php echo search_form(); ?>
