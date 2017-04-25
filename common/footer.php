6da75b43e0bfb13a0da685037cbe1d4264db69c2
dt-ucsd <dtucsd+git@gmail.com>
2017-04-25 14:11:55 -0700

    Fixes #3 - Update theme footer
    Added Library logo to footer. Placed it on the right side. Left side
    container can still take custom HTML via theme interface control.

--- common/footer.php
+++ common/footer.php
@@ -2,18 +2,18 @@
 
 <footer role="contentinfo">
 
-        <div id="custom-footer-text">
-            <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
-            <p><?php echo $footerText; ?></p>
-            <?php endif; ?>
-            <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
-                <p><?php echo $copyright; ?></p>
-            <?php endif; ?>
-        </div>
+	<div id="ucsdlib-footer">
+		<div id="custom-footer-text">
+			<?php if ( $footerText = get_theme_option('Footer Text') ): ?>
+				<?php echo $footerText; ?>
+			<?php endif; ?>
+		</div>
+		<div id="library-logo">
+			<a href='https://library.ucsd.edu'><img src="<?php echo img('library-logo-white.png'); ?>" alt="UC San Diego Library"></a>
+		</div>
+	</div>
 
-        <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
-
-    <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
+	<?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
 
 </footer>
 
