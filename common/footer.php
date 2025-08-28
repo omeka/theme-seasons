c142c929e268446dd2f88da8867119516c7bbe6a
Kim Nguyen <design@kimisgold.com>
2025-08-28 14:56:02 -0400

    Revert "Reorder footer content."
    This reverts commit 929ea19d18ce197cb35cfa51695dd121970ba8de.

--- common/footer.php
+++ common/footer.php
@@ -6,17 +6,14 @@
             <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
             <p><?php echo $footerText; ?></p>
             <?php endif; ?>
+            <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+                <p><?php echo $copyright; ?></p>
+            <?php endif; ?>
         </div>
-
-        <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
 
         <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
 
-        <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
-            <p><?php echo $copyright; ?></p>
-        <?php endif; ?>
-
-
+    <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
 
 </footer>
 
