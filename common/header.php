bcd8156b34e868e59bc5e3465c679f28705492cf
Kim Nguyen <design@kimisgold.com>
2026-07-30 14:50:40 -0400

    Use local fonts.

--- common/header.php
+++ common/header.php
@@ -28,7 +28,6 @@
 
     <!-- Stylesheets -->
     <?php
-    queue_css_url('//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic');
     queue_css_file(array('iconfonts', 'normalize', 'style'), 'screen');
     queue_css_file('print', 'print');
     echo head_css();
