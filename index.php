e23492ef66bd656ba56aa0c1517125669e021a74
Kim Nguyen <design@kimisgold.com>
2026-08-26 16:19:43 -0400

    Change h1 on homepage to 'Home'.
    Avoids scenario where no featured records are configured.
    
    (omeka/Omeka#1059)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <p><?php echo get_theme_option('Homepage Text'); ?></p>
 <?php endif; ?>
 
-<h1 class="sr-only"><?php echo __('Featured'); ?></h1>
+<h1 class="sr-only"><?php echo __('Home'); ?></h1>
 
 <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
 <!-- Featured Item -->
